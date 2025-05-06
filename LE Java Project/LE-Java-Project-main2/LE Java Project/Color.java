f100a2144ca6de87ebaee42103bc8277d27c4afa
Mr.P074to <150507652+Mr-Po74to-OTATO@users.noreply.github.com>
2025-05-06 09:25:48 +0800

    Add files via upload

---
+++ LE Java Project/LE-Java-Project-main2/LE Java Project/Color.java
@@ -0,0 +1,3 @@
+public abstract class Color {
+    private abstract int color;
+}
---
+++ LE Java Project/LE-Java-Project-main2/LE Java Project/HairStyle.java
@@ -0,0 +1,67 @@
+class Hair extends class Color {
+    public void HairStyle(int  Style, int color) {
+        boolean valid = false;
+
+        do {
+            System.out.println("Pick your HairStle.");
+
+            switch(Style) {
+                case 1:
+                    System.out.println("Hair: Wavy");
+                    valid = true;
+                    break;
+                case 2:
+                    System.out.println("Hair: Straight");
+                    valid = true;
+                    break;
+                case 3:
+                    System.out.println("Hair: Ponytail");
+                    valid = true;
+                    break;
+                case 4:
+                    System.out.println("Hair: Pig Tail");
+                    valid = true;
+                    break;
+                case 5:
+                    System.out.println("Hair: Curly Hair");
+                    valid = true;
+                    break;
+                default:
+                    System.out.println("Invalid Input. Try Again.");
+                    valid = false;
+                    break;
+            }
+        } while (valid);
+        
+        do {
+            System.out.println("Choose your preferred color.");
+
+            switch(color) {
+                case 1:
+                    System.out.println("Color: Red");
+                    valid = true;
+                    break;
+                case 2:
+                    System.out.println("Color: Blue");
+                    valid = true;
+                    break;
+                case 3:
+                    System.out.println("Color: Torquise");
+                    valid = true;
+                    break;
+                case 4:
+                    System.out.println("Color: Blonde");
+                    valid = true;
+                    break;
+                case 5:
+                    System.out.println("Color: Ash");
+                    valid = true;
+                    break;
+                default:
+                    System.out.println("Invalid Input. Try Again.");
+                    valid = false;
+                    break;
+            }
+        } while(valid);
+    }
+}
